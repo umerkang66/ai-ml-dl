74d64aee97e82a3d5a0f2d284a79547d59025dba
Muhammad Umer <umerkang66@gmail.com>
2026-07-09 18:12:53 +0500

    data transformation done

--- ml-ds-krish-naik/18-end-to-end-project-1/src/components/data_ingestion.py
+++ ml-ds-krish-naik/18-end-to-end-project-1/src/components/data_ingestion.py
@@ -1,11 +1,13 @@
 import os
 import sys
-from src.exception import CustomException
-from src.logger import logging
-import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+import pandas as pd
+
+from src.components.data_transformation import DataTransformation
+from src.exception import CustomException
+from src.logger import logging
 
 
 @dataclass
@@ -56,4 +58,11 @@ class DataIngestion:
 
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data_path, test_data_path = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    train_data, test_data, preprocessor_path = (
+        data_transformation.initiate_data_transformation(
+            train_data_path, test_data_path
+        )
+    )
