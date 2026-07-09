225c74002f0bf48fc55bcba0d8ad35fec111c20a
Muhammad Umer <umerkang66@gmail.com>
2026-07-09 21:52:44 +0500

    add train pipeline

--- ml-ds-krish-naik/18-end-to-end-project-1/src/components/data_ingestion.py
+++ ml-ds-krish-naik/18-end-to-end-project-1/src/components/data_ingestion.py
@@ -5,8 +5,6 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 import pandas as pd
 
-from src.components.data_transformation import DataTransformation
-from src.components.model_trainer import ModelTrainer
 
 from src.exception import CustomException
 from src.logger import logging
@@ -56,18 +54,3 @@ class DataIngestion:
         except Exception as e:
             logging.error(f"Error occurred while reading the dataset: {e}")
             raise CustomException(e, sys)
-
-
-if __name__ == "__main__":
-    obj = DataIngestion()
-    train_data_path, test_data_path = obj.initiate_data_ingestion()
-
-    data_transformation = DataTransformation()
-    train_data, test_data, preprocessor_path = (
-        data_transformation.initiate_data_transformation(
-            train_data_path, test_data_path
-        )
-    )
-
-    trainer = ModelTrainer()
-    print(trainer.initiate_model_trainer(train_data, test_data))
